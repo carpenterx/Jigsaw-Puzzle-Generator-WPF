ae3681c346c26f58dcf64e8f0728f128fcf31a97
Sorin <jordache_sor@yahoo.com>
2021-06-24 15:28:16 +0300

    added image loading
    fixes #1

--- Jigsaw Puzzle Generator WPF/MainWindow.xaml.cs
+++ Jigsaw Puzzle Generator WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Jigsaw_Puzzle_Generator_WPF
 {
@@ -20,9 +22,40 @@ namespace Jigsaw_Puzzle_Generator_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string IMAGE_PATH = @"C:\Users\jorda\Desktop\pexels-julia-volk-5273517.jpg";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ShowImage(IMAGE_PATH);
+        }
+
+        private void BrowseToImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".jpg"; // Default file extension
+            dlg.Filter = "JPG documents (.jpg)|*.jpg"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                ShowImage(dlg.FileName);
+            }
+        }
+
+        private void ShowImage(string imagePath)
+        {
+            BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
+            puzzleCanvas.Width = bitmapImage.PixelWidth;
+            puzzleCanvas.Height = bitmapImage.PixelHeight;
+
+            image.Source = bitmapImage;
+            image.Width = bitmapImage.PixelWidth;
+            image.Height = bitmapImage.PixelHeight;
         }
     }
 }
